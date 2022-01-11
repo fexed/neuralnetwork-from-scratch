567f59d70e0fed0715c1694ddf940d71d2d65c83
Federico Matteoni <fedex15@live.it>
2022-01-11 17:50:48 +0100

    Fix

--- nntest_monk_gridsearch.py
+++ nntest_monk_gridsearch.py
@@ -14,6 +14,6 @@ X = np.array(xtr)
 Y = np.array(ytr)
 xtr, xvl, ytr, yvl = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-grid_search(6, 1, xtr, ytr, X_validation=xvl, Y_validation=yvl, layers=[1,2,3,4,5], units=[5,10,15,20], learning_rates=[0.005, 0.01, 0.1], batch_sizes=[None, xtr.size()], epochs=500)
+grid_search(6, 1, xtr, ytr, X_validation=xvl, Y_validation=yvl, layers=[1,2,3,4,5], units=[5,10,15,20], learning_rates=[0.005, 0.01, 0.1], batch_sizes=[None, np.size(xtr)], epochs=500)
 
 # Best: 0.00005 with {'layers': 5, 'units': 15, 'learning_rate': 0.01}
