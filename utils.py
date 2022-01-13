2a4a3af8b6bfed52a2feb7cd51f05b071b6d510c
Federico Matteoni <fedex15@live.it>
2022-01-13 11:14:00 +0100

    Typo

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def update_progress(progress, barlength=90, suffix="", fill="#"):
     num = int(round(barlength*progress))
-    txt = "\r" + suffix + " [" + "#"*num + " "*(barlength - num) + "] " + "{:.2f}".format(progress*100) + "%"
+    txt = "\r" + suffix + " [" + fill*num + " "*(barlength - num) + "] " + "{:.2f}".format(progress*100) + "%"
     print(txt, end="")
 
 
