bf368398447bb7cb07a908b2f41bbfa7ef6fb053
Federico Matteoni <fedex15@live.it>
2022-01-15 13:25:51 +0100

    Update nntest_monk.py

--- nntest_monk.py
+++ nntest_monk.py
@@ -11,7 +11,7 @@ from kfold import KFold
 from preprocessing import one_hot_encoding
 from regularizators import L2
 
-def test_MONK(monk=1, output=True, use_one_hot_encoding = True):
+def test_MONK(monk=1, output=True, use_one_hot_encoding=True):
     ts = str(time.time()).split(".")[0]  # current timestamp for log purposes
     if (output): print("\n\n****MONK" + str(monk))
     monkfile = open("datasets/MONK/monks-" + str(monk) + ".train", "r")
@@ -23,48 +23,48 @@ def test_MONK(monk=1, output=True, use_one_hot_encoding = True):
         ytr.append([[int(vals[1])]])
     X = np.array(xtr)
     Y = np.array(ytr)
-    
+
     input_size = 6
 
     if use_one_hot_encoding:
         X, input_size = one_hot_encoding(X)
 
-    #xtr, xvl, ytr, yvl = train_test_split(X, Y, test_size=0.2, random_state=42)
     if (output): print("Training set of " + str(X.size) + " elements")
-    folds = 3
-    net = Network("MONK" + str(monk) + " " + str(folds) + "-fold test", binary_crossentropy, binary_crossentropy_prime, regularizator=L2, regularization_l=0.005, momentum=0.5)
-    net.add(FullyConnectedLayer(input_size, 10, sigmoid, sigmoid_prime, initialization_func="normalized_xavier"))
-    net.add(FullyConnectedLayer(10, 1, sigmoid, sigmoid_prime, initialization_func="normalized_xavier"))
+    folds = 1
+    net = Network("MONK" + str(monk), binary_crossentropy, binary_crossentropy_prime, momentum=0)
+    net.add(FullyConnectedLayer(input_size, 5, sigmoid, sigmoid_prime, initialization_func="normalized_xavier"))
+    net.add(FullyConnectedLayer(5, 5, sigmoid, sigmoid_prime, initialization_func="normalized_xavier"))
+    net.add(FullyConnectedLayer(5, 1, sigmoid, sigmoid_prime, initialization_func="normalized_xavier"))
     # train
     if (output): net.summary()
     mean_accuracy = 0 #mean accuracy over the kfolds
-    kfold = KFold(folds, X, Y)
+    #kfold = KFold(folds, X, Y)
     suffix = "MONK" + str(monk) + "_" + ts
     fig, ax = plot.subplots()
-    while (kfold.hasNext()):
-        xtr, xvl, ytr, yvl = kfold.next_fold()
-        history, val_history = net.training_loop(xtr, ytr, X_validation=xvl, Y_validation=yvl, epochs=1000, learning_rate=0.01, verbose=output, early_stopping=25)
+    #while (kfold.hasNext()):
+    xtr, xvl, ytr, yvl = train_test_split(X, Y, test_size=0.2, random_state=42)
+    history, val_history = net.training_loop(xtr, ytr, X_validation=xvl, Y_validation=yvl, epochs=1000, learning_rate=0.005, verbose=output, early_stopping=25)
 
-        # accuracy on validation set
-        out = net.predict(xvl)
-        accuracy = 0
-        for i in range(len(out)):
-            val = 0 if out[i].item() < 0.5 else 1  # "normalizing" output
-            if (yvl[i].item() == val): accuracy += 1
-        accuracy /= len(out)
-        accuracy *= 100
-        mean_accuracy += accuracy
-        if (output): print("\n\nAccuracy on MONK" + str(monk) + " validation set of {:.4f}%".format(accuracy) + " over " + str(len(out)) + " elements")
+    # accuracy on validation set
+    out = net.predict(xvl)
+    accuracy = 0
+    for i in range(len(out)):
+        val = 0 if out[i].item() < 0.5 else 1  # "normalizing" output
+        if (yvl[i].item() == val): accuracy += 1
+    accuracy /= len(out)
+    accuracy *= 100
+    mean_accuracy += accuracy
+    if (output): print("Accuracy on MONK" + str(monk) + " validation set of {:.4f}%".format(accuracy) + " over " + str(len(out)) + " elements")
 
-        ax.plot(history)
-        #ax.plot(val_history)
-        ax.set_ylabel("Loss")
-        ax.set_xlabel("Epochs")
-        ax.set_title(suffix)
-        #with open("logs/" + suffix + "_history.pkl", "wb") as logfile:
-            #pickle.dump(history, logfile)
-        #with open("logs/" + suffix + "_valhistory.pkl", "wb") as logfile:
-            #pickle.dump(val_history, logfile)
+    ax.plot(val_history)
+    #ax.plot(val_history)
+    ax.set_ylabel("Val Loss")
+    ax.set_xlabel("Epochs")
+    ax.set_title(suffix)
+    #with open("logs/" + suffix + "_history.pkl", "wb") as logfile:
+        #pickle.dump(history, logfile)
+    #with open("logs/" + suffix + "_valhistory.pkl", "wb") as logfile:
+        #pickle.dump(val_history, logfile)
 
     mean_accuracy /= folds
     if (output): print("\n\nMean accuracy over " + str(folds) + " folds: {:.4f}%".format(mean_accuracy))
@@ -79,9 +79,9 @@ def test_MONK(monk=1, output=True, use_one_hot_encoding = True):
 print("Beginning tests")
 for i in range(1, 4):
     acc = []
-    for j in range(0, 10):
-        acc.append(test_MONK(i, output=False))
-        print(str(j+1), end=" ", flush=True)
+    for j in range(0, 1):
+        acc.append(test_MONK(i, output=True))
+        #print(str(j+1), end=" ", flush=True)
     print("")
     print("MONK" + str(i), end=" ")
     mean = np.mean(acc)
