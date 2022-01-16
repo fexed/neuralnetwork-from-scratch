Fixed backpropagation

layers.py
@@ -70,8 +70,8 @@ class FullyConnectedLayer(Layer):
             # derivative
             gradient = np.multiply(self.activation_prime(self.activation_input), gradient)
         # the weights are updated according to their contribution to the error
-        weights_update = eta * np.dot(self.input.T, gradient)
-        bias_update = eta * gradient
+        weights_update = -eta * np.dot(self.input.T, gradient)
+        bias_update = -eta * gradient
 
         if (momentum > 0):
             # with momentum we consider the previous update too
@@ -88,8 +88,8 @@ class FullyConnectedLayer(Layer):
 
         # the basic parameter update
         # TODO check overflow situations
-        self.weights -= weights_update
-        self.bias -= bias_update
+        self.weights += weights_update
+        self.bias += bias_update
 
         input_error = np.dot(gradient, self.weights.T)
         return input_error