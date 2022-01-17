df20c8510b150396c9957aee839c104e5ba52a5d
Federico Matteoni <fedex15@live.it>
2022-01-17 12:11:52 +0100

    Update nntest_cup.py

--- nntest_cup.py
+++ nntest_cup.py
@@ -1,14 +1,17 @@
-import numpy as np
-from activationfunctions import tanh, tanh_prime, sigmoid, sigmoid_prime
+from activationfunctions import sigmoid, sigmoid_prime
 from losses import MSE, MSE_prime
 from layers import FullyConnectedLayer
 from neuralnetwork import Network
-import matplotlib.pyplot as plot
-import time
-import pickle
 from kfold import KFold
 from preprocessing import continuous_standardizer
 from regularizators import L2
+from dataset_loader import load_cup
+from preprocessing import continuous_standardizer, min_max_normalizer
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plot
+import time
+import pickle
 
 
 def compare(a, b, tollerance=1e-03):
@@ -18,64 +21,42 @@ def compare(a, b, tollerance=1e-03):
 def test_CUP(output=True):
     ts = str(time.time()).split(".")[0]  # current timestamp for log purposes
     if (output): print("\n\n****CUP")
-    cupfile = open("datasets/CUP/ML-CUP21-TR.csv", "r")
-    xtr = []
-    ytr = []
-    for line in cupfile.readlines():
-        if (line.startswith("#")):
-            continue
-        vals = line.split(",")
-        xtr.append([[float(vals[1]), float(vals[2]), float(vals[3]), float(vals[4]), float(vals[5]), float(vals[6]), float(vals[7]), float(vals[8]), float(vals[9]), float(vals[10])]])
-        ytr.append([[float(vals[11]), float(vals[12])]])
-    X, m, s = continuous_standardizer(np.array(xtr))
-    Y = np.array(ytr)
-    if (output): print("Training set of " + str(np.size(X)) + " elements")
-    folds = 5
+    X, Y = load_cup(verbose=True, test=False)
     # train
-    #if (output): net.summary()
-    mean_accuracy = 0 #mean accuracy over the kfolds
-    kfold = KFold(folds, X, Y)
+    X, n_min, n_max = min_max_normalizer(X)
+    X, means, std = continuous_standardizer(X)
+    xtr, xvl, ytr, yvl = train_test_split(X, Y, test_size=0.2, random_state=42)
     suffix = "CUP_" + ts
     fig, ax = plot.subplots()
-    while (kfold.hasNext()):
-        net = Network("CUP " + str(folds) + "-fold test", MSE, MSE_prime, regularizator=L2, regularization_l=0.005, momentum=0.8)        
-        net.add(FullyConnectedLayer(10, 20, tanh, tanh_prime, initialization_func="normalized_xavier"))
-        net.add(FullyConnectedLayer(20, 20, tanh, tanh_prime, initialization_func="normalized_xavier"))
-        net.add(FullyConnectedLayer(20, 20, tanh, tanh_prime, initialization_func="normalized_xavier"))
-        net.add(FullyConnectedLayer(20, 2, initialization_func="normalized_xavier"))
-        xtr, xvl, ytr, yvl = kfold.next_fold()
-        history, val_history = net.training_loop(xtr, ytr, X_validation=xvl, Y_validation=yvl, epochs=600, learning_rate=0.1, verbose=output, early_stopping=50)
+    net = Network("CUP test", MSE, MSE_prime, regularizator=L2, regularization_l=0.005)
+    net.add(FullyConnectedLayer(10, 30, sigmoid, sigmoid_prime, initialization_func="normalized_xavier"))
+    net.add(FullyConnectedLayer(30, 30, sigmoid, sigmoid_prime, initialization_func="normalized_xavier"))
+    net.add(FullyConnectedLayer(30, 30, sigmoid, sigmoid_prime, initialization_func="normalized_xavier"))
+    net.add(FullyConnectedLayer(30, 30, sigmoid, sigmoid_prime, initialization_func="normalized_xavier"))
+    net.add(FullyConnectedLayer(30, 30, sigmoid, sigmoid_prime, initialization_func="normalized_xavier"))
+    net.add(FullyConnectedLayer(30, 2, initialization_func="normalized_xavier"))
+    if (output): net.summary()
+    history, val_history = net.training_loop(xtr, ytr, X_validation=xvl, Y_validation=yvl, epochs=10000, learning_rate=0.001, verbose=output, early_stopping=50, batch_size=1)
 
-        # accuracy on validation set
-        out = net.predict(xvl)
-        accuracy = 0
-        print(yvl[0], " ", out[0])
-        for i in range(len(out)):
-            # if (yvl[i][0][0] == out[i][0][0] and yvl[i][0][1] == out[i][0][1]): accuracy += 1
-            if (compare(yvl[i][0][0], out[i][0][0]) and compare(yvl[i][0][1], out[i][0][1])): accuracy += 1
-        accuracy /= len(out)
-        accuracy *= 100
-        mean_accuracy += accuracy
-        if (output): print("\n\nAccuracy on CUP validation set of {:.4f}%".format(accuracy) + " over " + str(len(out)) + " elements")
+    # accuracy on validation set
+    out = net.predict(xvl)
+    accuracy = 0
+    for i in range(len(out)):
+        # if (yvl[i][0][0] == out[i][0][0] and yvl[i][0][1] == out[i][0][1]): accuracy += 1
+        if (compare(yvl[i][0][0], out[i][0][0]) and compare(yvl[i][0][1], out[i][0][1])): accuracy += 1
+    accuracy /= len(out)
+    accuracy *= 100
 
-        ax.plot(history)
-        #ax.plot(val_history)
-        ax.set_ylabel("Loss")
-        ax.set_xlabel("Epochs")
-        ax.set_title(suffix)
-        #with open("logs/" + suffix + "_history.pkl", "wb") as logfile:
-            #pickle.dump(history, logfile)
-        #with open("logs/" + suffix + "_valhistory.pkl", "wb") as logfile:
-            #pickle.dump(val_history, logfile)
+    if (output): print("Accuracy: {:.4f}%".format(accuracy))
 
-    mean_accuracy /= folds
-    if (output): print("\n\nMean accuracy over " + str(folds) + " folds: {:.4f}%".format(mean_accuracy))
-
+    ax.plot(history)
+    ax.set_ylabel("Loss")
+    ax.set_xlabel("Epochs")
+    ax.set_title(suffix)
     plot.gca().margins(x=0)
     fig.set_size_inches(18.5, 10.5)
-    plot.savefig("plots/" + suffix + "_" + str(folds) + "folds_history.png")
+    plot.savefig("plots/" + suffix + "_history.png")
     plot.clf()
-    return mean_accuracy
 
 
 print("Beginning tests\n")
