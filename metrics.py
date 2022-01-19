0a94a544cd90812d5a4925b3195febc86918ff05
Federico Matteoni <fedex15@live.it>
2022-01-19 10:44:13 +0100

    Handle single target

--- metrics.py
+++ metrics.py
@@ -5,7 +5,9 @@ def accuracy(model, dataset, targets, tollerance = 1e-03):
     out = model.predict(dataset)  # get the predictions
     accuracy = 0
     for i in range(len(out)):
-        if (targets[0].shape[1] == 2):  # if CUP
+        if len(targets[0].shape) == 1:  # if just one target
+            if (compare(targets[i][0], out[i][0][0], tollerance = tollerance)): accuracy += 1
+        elif (targets[0].shape[1] == 2):  # if normal CUP
             if (compare(targets[i][0][0], out[i][0][0], tollerance = tollerance) and compare(targets[i][0][1], out[i][0][1], tollerance = tollerance)): accuracy += 1
         else:  # else MONK
             val = 0 if out[i].item() < 0.5 else 1
