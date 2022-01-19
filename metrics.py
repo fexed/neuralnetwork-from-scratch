f3d37f15c2d3f4d69b96652ec3955e53bea1166a
Federico Matteoni <fedex15@live.it>
2022-01-19 10:32:43 +0100

    Fix (#35)

--- metrics.py
+++ metrics.py
@@ -5,7 +5,11 @@ def accuracy(model, dataset, targets, tollerance = 1e-03):
     out = model.predict(dataset)
     accuracy = 0
     for i in range(len(out)):
-        # if (yvl[i][0][0] == out[i][0][0] and yvl[i][0][1] == out[i][0][1]): accuracy += 1
-        if (compare(targets[i][0][0], out[i][0][0], tollerance = tollerance) and compare(targets[i][0][1], out[i][0][1], tollerance = tollerance)): accuracy += 1
+        if (targets[0].shape[1] == 2):
+            if (compare(targets[i][0][0], out[i][0][0], tollerance = tollerance) and compare(targets[i][0][1], out[i][0][1], tollerance = tollerance)): accuracy += 1
+        else:
+            val = 0 if out[i].item() < 0.5 else 1
+            if (targets[i].item() == val): accuracy += 1
     accuracy /= len(out)
     accuracy *= 100
+    return accuracy
