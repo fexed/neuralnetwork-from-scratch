180cd553229db2bd18efb4da986e542d106e8580
Federico Matteoni <fedex15@live.it>
2022-01-19 10:36:46 +0100

    Comments #30

--- metrics.py
+++ metrics.py
@@ -2,12 +2,12 @@ from utils import compare
 
 
 def accuracy(model, dataset, targets, tollerance = 1e-03):
-    out = model.predict(dataset)
+    out = model.predict(dataset)  # get the predictions
     accuracy = 0
     for i in range(len(out)):
-        if (targets[0].shape[1] == 2):
+        if (targets[0].shape[1] == 2):  # if CUP
             if (compare(targets[i][0][0], out[i][0][0], tollerance = tollerance) and compare(targets[i][0][1], out[i][0][1], tollerance = tollerance)): accuracy += 1
-        else:
+        else:  # else MONK
             val = 0 if out[i].item() < 0.5 else 1
             if (targets[i].item() == val): accuracy += 1
     accuracy /= len(out)
