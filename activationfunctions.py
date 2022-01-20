ef8d7f9c843f1bfee2d8699c8040b9e5f30cf0d1
Federico Matteoni <fedex15@live.it>
2022-01-20 15:01:55 +0100

    Commenting (#30)

--- activationfunctions.py
+++ activationfunctions.py
@@ -2,76 +2,109 @@ import numpy as np
 
 
 class ActivationFunction:
+    """ Base class for the activation functions """
+
     def __init__(self):
         self.name = None
 
 
 class Tanh(ActivationFunction):
+    """ Tanh activation function """
+
     def __init__(self):
         self.name = "Tanh"
 
 
-    def forward(self, x):  # returns the tanh value of x
+    def forward(self, x):
+        """ Returns the tanh value of x """
+
         return np.tanh(x)
 
 
-    def derivative(self, x):  # derivative of the tanh function
+    def derivative(self, x):
+        """ Derivative of the tanh function """
+
         return 1 - np.power(np.tanh(x), 2)
 
 
 class Sigmoid(ActivationFunction):
+    """ Sigmoid activation function """
+
     def __init__(self):
         self.name = "Sigmoid"
 
 
-    def forward(self, x):  # returns the sigmoid value of x
-        return 1 / (1 + np.exp(np.dot(-1, x)))  # 1/(1+e^{-x})
+    def forward(self, x):
+        """ Returns the sigmoid value of x """
+
+        # 1/(1+e^{-x})
+        return 1 / (1 + np.exp(np.dot(-1, x)))
 
 
-    def derivative(self, x):  # derivative of the sigmoid function
+    def derivative(self, x):
+        """ Derivative of the sigmoid function """
+
         sigma = self.forward(x)
         return sigma * (1 - sigma)
 
 
 class ReLU(ActivationFunction):
+    """ ReLU activation function """
+
     def __init__(self):
         self.name = "ReLU"
 
 
-    def forward(self, x):  # returns the positive part of x
+    def forward(self, x):
+        """ Returns the positive part of x """
         return np.maximum(x, 0)
 
 
-    def derivative(self, x):  # derivative of the ReLU function
+    def derivative(self, x):
+        """ Derivative of the ReLU function """
+
         return np.array(x > 0).astype('int')
 
 
-class ReLU(ActivationFunction):
+class LeakyReLU(ActivationFunction):
+    """ Leaky ReLU activation function """
+
     def __init__(self, leak = 0.3):
         self.name = "ReLU"
         self.leak = leak
 
 
     def forward(self, x):
-        # returns the positive part of x or allows a small gradient if x <= 0
+        """ Returns the positive part of x or allows a
+        small gradient if x <= 0
+        """
+
         return ((x > 0) * x) + ((x <= 0) * self.leak * x)
 
 
-    def derivative(self, x):  # derivative of the leaky ReLU function
+    def derivative(self, x):
+        """ Derivative of the leaky ReLU function """
+
         return (x > 0) + ((x <= 0) * self.leak)
 
 
 class Softmax(ActivationFunction):  # TODO: check this
+    """ Softmax activation function """
+
     def __init__(self):
         self.name = "Softmax"
 
 
-    def forward(self, x):  # return the softmax value of x
+    def forward(self, x):
+        """ Returns the softmax value of x """
+
         exp = np.exp(x - x.max())  # subtract max for numerical stability
         return exp / exp.sum(axis = 0)
 
 
-    def derivative(self, x):  # derivative of the softmax function
+    def derivative(self, x):
+        """ Derivative of the softmax function """
+        
         jacobian_m = np.diag(x)
         s = []
 
