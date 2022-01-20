ab258ca2752e0e1a6471285d932f7c0e0e9db9e0
Federico Matteoni <fedex15@live.it>
2022-01-20 15:12:36 +0100

    Commenting (#30)

--- regularizators.py
+++ regularizators.py
@@ -2,11 +2,15 @@ import numpy as np
 
 
 class Regularizator():
+        """ Base class for the regularation functions """
 
         def __init__(self, lambda):
             self.name = None
             self.lambda = 0
+
+
 class L2(Regularizator):
+        """ L2 regularizator """
 
         def __init__(self, l = 0.005):
             self.name = L2
