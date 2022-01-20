95402e1252ba4c73c8b5f6816dad9467fec866ff
Federico Matteoni <fedex15@live.it>
2022-01-20 15:12:26 +0100

    Regularizators to class (#30)

--- regularizators.py
+++ regularizators.py
@@ -1,4 +1,16 @@
 import numpy as np
 
-def L2(weights, l=0.005): #L2 norm regularization
-    return 2*l*weights
+
+class Regularizator():
+
+        def __init__(self, lambda):
+            self.name = None
+            self.lambda = 0
+class L2(Regularizator):
+
+        def __init__(self, l = 0.005):
+            self.name = L2
+            self.lambda = l
+
+        def apply(self, weights):
+            return 2 * self.lambda * weights
