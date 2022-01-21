1070d058516511c2eb8800a714b70811a96a41c6
Federico Matteoni <fedex15@live.it>
2022-01-21 19:05:02 +0100

    A better accuracy metric for MONK

--- metrics.py
+++ metrics.py
@@ -21,14 +21,16 @@ class Accuracy(Metric):
                 elif (targets[0].shape[1] == 2):  # if normal CUP
                     if (compare(targets[i][0][0], out[i][0][0], tollerance = self.tollerance) and compare(targets[i][0][1], out[i][0][1], tollerance = self.tollerance)): accuracy += 1
                 else:  # else MONK
-                    val = 0 if out[i].item() < 0.5 else 1
-                    if (targets[i].item() == val): accuracy += 1
+                    accuracy += (1-abs(out[i].item() - targets[i].item()))
+                    #val = 0 if out[i].item() < 0.5 else 1
+                    #if (targets[i].item() == val): accuracy += 1
             else:
                 if (self.dataset == "CUP"):
                     if (compare(targets[i][0][0], out[i][0][0], tollerance = self.tollerance) and compare(targets[i][0][1], out[i][0][1], tollerance = self.tollerance)): accuracy += 1
                 elif (self.dataset == "MONK"):
-                    val = 0 if out[i].item() < 0.5 else 1
-                    if (targets[i].item() == val): accuracy += 1
+                    accuracy += (1-abs(out[i].item() - targets[i].item()))
+                    #val = 0 if out[i].item() < 0.5 else 1
+                    #if (targets[i].item() == val): accuracy += 1
                 else:
                     if (compare(targets[i][0], out[i][0][0], tollerance = self.tollerance)): accuracy += 1
         accuracy /= len(out)
