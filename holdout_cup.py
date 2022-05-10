ef57c3d858f0bc5b0766882c48c45d0e70a2985a
Sergio <Sergio.ltr96@gmail.com>
2022-05-10 18:57:24 +0200

    Print Value 1, Value 2

--- holdout_cup.py
+++ holdout_cup.py
@@ -1,5 +1,5 @@
 from random import random
-from activationfunctions import Tanh
+from activationfunctions import LeakyReLU, ReLU, Sigmoid, Tanh
 from losses import MSE
 from layers import FullyConnectedLayer
 from neuralnetwork import Network
@@ -40,14 +40,34 @@ history, validation_history = net.training_loop(X_TR, Y_TR ,X_validation=X_VAL, 
 # evaluating
 out = net.predict(X_VAL)
 accuracy = 0
+acc1=0
+acc2=0
+
 print(Y_VAL[0], " ", out[0])
 for i in range(len(out)):
     # if (yvl[i][0][0] == out[i][0][0] and yvl[i][0][1] == out[i][0][1]): accuracy += 1
     if (compare(Y_VAL[i][0][0], out[i][0][0]) and compare(Y_VAL[i][0][1], out[i][0][1])): accuracy += 1
+
+    #Delete from here
+    if compare(Y_VAL[i][0][0], out[i][0][0]): acc1 +=1
+    if compare(Y_VAL[i][0][1], out[i][0][1]): acc2 +=1
+
+acc1 /= len(out)
+acc1 *= 100
+
+acc2 /= len(out)
+acc2 *= 100
+#to here
+
 accuracy /= len(out)
 accuracy *= 100
 
 print("\n\nAccuracy on CUP validation set of {:.4f}%".format(accuracy) + " over " + str(len(out)) + " elements")
 
+print("\n\nAccuracy on VAL1 ", acc1 ,"%")
+print("Accuracy on VAL2 ", acc2, "%")
+
 # plotting data
 plot_and_save(title="CUP model evaluation", history=history, validation_history=validation_history, ylabel="Loss", xlabel="Epochs", savefile="CUP_HOLDOUT")
+#plot_and_save(title="CUP target1 evaluation", history=history, validation_history=validation_history, ylabel="Loss", xlabel="Epochs", savefile="CUP_HOLDOUT")
+#plot_and_save(title="CUP target2 evaluation", history=history, validation_history=validation_history, ylabel="Loss", xlabel="Epochs", savefile="CUP_HOLDOUT")
