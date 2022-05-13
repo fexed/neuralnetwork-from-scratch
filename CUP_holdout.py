6ba93b7dc0526decf6d09939d5537b9d512579d7
Federico Matteoni <fedex15@live.it>
2022-05-13 15:26:05 +0200

    Renaming and reworking

--- CUP_holdout.py
+++ CUP_holdout.py
@@ -2,6 +2,7 @@ from random import random
 from activationfunctions import LeakyReLU, ReLU, Sigmoid, Tanh
 from losses import MSE
 from layers import FullyConnectedLayer
+from metrics import MeanEuclideanError
 from neuralnetwork import Network
 from utils import plot_and_save, tr_vl_split, compare
 import numpy as np
@@ -28,8 +29,6 @@ X_TR,  X_VAL, Y_TR, Y_VAL = tr_vl_split(X, Y, ratio=0.2)
 net = Network("CUP", MSE())
 net.add(FullyConnectedLayer(10, 25, Tanh(), initialization_func="normalized_xavier"))
 net.add(FullyConnectedLayer(25, 25, Tanh(), initialization_func="normalized_xavier"))
-net.add(FullyConnectedLayer(25, 25, Tanh(), initialization_func="normalized_xavier"))
-net.add(FullyConnectedLayer(25, 25, Tanh(), initialization_func="normalized_xavier"))
 net.add(FullyConnectedLayer(25, 2,  initialization_func="normalized_xavier"))
 net.summary()
 history, validation_history = net.training_loop(X_TR, Y_TR ,X_validation=X_VAL, Y_validation=Y_VAL, epochs=150, learning_rate=0.001, verbose=True, batch_size=1)
@@ -43,7 +42,7 @@ accuracy = 0
 acc1=0
 acc2=0
 
-print(Y_VAL[0], " ", out[0])
+# print(Y_VAL[0], " ", out[0])
 for i in range(len(out)):
     # if (yvl[i][0][0] == out[i][0][0] and yvl[i][0][1] == out[i][0][1]): accuracy += 1
     if (compare(Y_VAL[i][0][0], out[i][0][0]) and compare(Y_VAL[i][0][1], out[i][0][1])): accuracy += 1
@@ -62,10 +61,13 @@ acc2 *= 100
 accuracy /= len(out)
 accuracy *= 100
 
+MEE = MeanEuclideanError().compute(net, X_VAL, Y_VAL)
+
 print("\n\nAccuracy on CUP validation set of {:.4f}%".format(accuracy) + " over " + str(len(out)) + " elements")
 
-print("\n\nAccuracy on VAL1 ", acc1 ,"%")
-print("Accuracy on VAL2 ", acc2, "%")
+print("\nAccuracy on VAL1", acc1 ,"%")
+print("Accuracy on VAL2", acc2, "%")
+print("MEE", MEE)
 
 # plotting data
 plot_and_save(title="CUP model evaluation", history=history, validation_history=validation_history, ylabel="Loss", xlabel="Epochs", savefile="CUP_HOLDOUT")
