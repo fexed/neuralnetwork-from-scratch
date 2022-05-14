be91ac6d47d79b72863dc17dcaa22cc5819e9c71
Federico Matteoni <fedex15@live.it>
2022-05-14 09:13:54 +0200

    Gridsearch parameters ( #44 )

--- CUP_gridsearch.py
+++ CUP_gridsearch.py
@@ -9,7 +9,7 @@ from utils import tr_vl_split
 X, Y = load_cup(verbose=True, test=False)
 xtr, xvl, ytr, yvl = tr_vl_split(X, Y, ratio = 0.25)
 
-res = grid_search(10, 2, xtr, ytr, X_validation=xvl, Y_validation=yvl, layers=list(range(1, 4)), units=list(range(5, 20, 5)), learning_rates=[0.001, 0.0001], batch_sizes=[1], init_functions=[None, "normalized_xavier"], momentums=[0.5, 0.8], regularizators=[None, L2], regularization_lambdas=[0.005, 0.15], epochs=1000, verbose=False, early_stopping=150)
+res = grid_search(10, 2, xtr, ytr, X_validation=xvl, Y_validation=yvl, layers=list(range(1, 4)), units=list(range(5, 20, 5)), learning_rates=[1e-3, 1e-4, 1e-5, 1e-6, 1e-7], batch_sizes=[1], init_functions=["normalized_xavier"], momentums=[0.8], regularizators=[None, L2(l = 0.001), L2(l = 0.005), L2(l = 0.010), L2(l = 0.015)], epochs=1000, verbose=False, early_stopping=150)
 result_file = open("datasets/CUP/grid_search/results.txt", "w")
 print("CUP\n")
 result_file.write("CUP:\n")
