76bfa1402e611b64f6463e92e9d9ecb1fab60f1d
Federico Matteoni <fedex15@live.it>
2022-05-20 17:33:29 +0200

    Now evaluating based on mean/stddev over N=10 runs

--- CUP_holdout.py
+++ CUP_holdout.py
@@ -11,65 +11,34 @@ import matplotlib.pyplot as plot
 from preprocessing import continuous_standardizer, min_max_normalizer
 from dataset_loader import load_cup
 
-print("\n\n****TESTING NETWORK ON CUP" )
+
+def CUP_evaluation():
+    # Training set loading
+    X, Y = load_cup()
+
+    # Preprocessing
+    X, n_min, n_max = min_max_normalizer(X)
+    X, means, std = continuous_standardizer(X)
 
 
-# Training
+    X_TR,  X_VAL, Y_TR, Y_VAL = tr_vl_split(X, Y, ratio=0.2)
 
-# training set loading +
-X, Y = load_cup()
+    # Training
+    net = Network("CUP", MEE(), regularizator=L2(l = 1e-06))
+    net.add(FullyConnectedLayer(10, 24, Tanh(), initialization_func="normalized_xavier"))
+    net.add(FullyConnectedLayer(24, 2,  initialization_func="normalized_xavier"))
+    history = net.training_loop(X_TR, Y_TR, epochs=1300, learning_rate=0.000625, verbose=False, batch_size=1)
 
-# preprocessing
-X, n_min, n_max = min_max_normalizer(X)
-X, means, std = continuous_standardizer(X)
+    # Model evaluation
+    err = MeanEuclideanError().compute(net, X_VAL, Y_VAL)
+    return err, net
 
 
-X_TR,  X_VAL, Y_TR, Y_VAL = tr_vl_split(X, Y, ratio=0.2)
+print("\n\n****EVALUATING NETWORK ON CUP" )
+vals = []
+for i in range(10):
+    err, net = CUP_evaluation()
+    vals.append(err)
 
-# training
-net = Network("CUP", MEE(), regularizator=L2(l = 1e-05))
-net.add(FullyConnectedLayer(10, 23, Tanh(), initialization_func="normalized_xavier"))
-net.add(FullyConnectedLayer(23, 2,  initialization_func="normalized_xavier"))
 net.summary()
-history, validation_history = net.training_loop(X_TR, Y_TR, X_validation=X_VAL, Y_validation=Y_VAL, epochs=1000, learning_rate=0.0025, verbose=True, batch_size=1, early_stopping=50)
-
-# Model evaluation
-
-
-# evaluating
-out = net.predict(X_VAL)
-accuracy = 0
-acc1=0
-acc2=0
-
-# print(Y_VAL[0], " ", out[0])
-for i in range(len(out)):
-    # if (yvl[i][0][0] == out[i][0][0] and yvl[i][0][1] == out[i][0][1]): accuracy += 1
-    if (compare(Y_VAL[i][0][0], out[i][0][0]) and compare(Y_VAL[i][0][1], out[i][0][1])): accuracy += 1
-
-    #Delete from here
-    if compare(Y_VAL[i][0][0], out[i][0][0]): acc1 +=1
-    if compare(Y_VAL[i][0][1], out[i][0][1]): acc2 +=1
-
-acc1 /= len(out)
-acc1 *= 100
-
-acc2 /= len(out)
-acc2 *= 100
-#to here
-
-accuracy /= len(out)
-accuracy *= 100
-
-MEE = MeanEuclideanError().compute(net, X_VAL, Y_VAL)
-
-print("\n\nAccuracy on CUP validation set of {:.4f}%".format(accuracy) + " over " + str(len(out)) + " elements")
-
-print("\nAccuracy on VAL1", acc1 ,"%")
-print("Accuracy on VAL2", acc2, "%")
-print("MEE", MEE)
-
-# plotting data
-plot_and_save(title="CUP model evaluation", history=history, validation_history=validation_history, ylabel="Loss", xlabel="Epochs", savefile="CUP_HOLDOUT")
-
-#net.savenet("models/CUP_currentbest.pkl")
+print("MEE", np.mean(vals), "+-", np.std(vals))
