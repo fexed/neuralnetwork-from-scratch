b6f7c484b9b55a4358d9e1ebabdc785b021577a5
Federico Matteoni <fedex15@live.it>
2022-05-25 14:14:29 +0200

    Testing with Nesterov only

--- CUP_gridsearch.py
+++ CUP_gridsearch.py
@@ -8,8 +8,8 @@ from utils import tr_vl_split
 
 X, Y = load_cup(verbose=True, file="training")
 
-res = grid_search(10, 2, X, Y, layers=[0], units=list(range(22, 26, 1)), learning_rates=[0.0025, 0.000625], batch_sizes=[1, 16, 32], init_functions=["normalized_xavier"], momentums=[0, 0.5, 0.8], regularizators=[L2(l = 1e-5), L2(l = 1e-6)], dropouts=[0,], nesterovs=[False, True], epochs=2000, verbose=False, early_stopping=150)
-result_file = open("datasets/CUP/grid_search/results.txt", "w")
+res = grid_search(10, 2, X, Y, layers=[0, 1], units=list(range(20, 24, 1)), learning_rates=[0.0025, 0.000625], batch_sizes=[16, 32], init_functions=["normalized_xavier"], momentums=[0.5, 0.8], regularizators=[L2(l = 1e-5), L2(l = 1e-6)], dropouts=[0], nesterovs=[True], epochs=2000, verbose=False, early_stopping=150)
+result_file = open("datasets/CUP/grid_search/results_nesterovtest.txt", "w")
 print("CUP\n")
 result_file.write("CUP:\n")
 for i in range (0, 10):
