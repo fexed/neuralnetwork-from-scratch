8d1b8da45d076bb5b962d3b5d8f8b75507150a86
Federico Matteoni <fedex15@live.it>
2022-05-28 09:32:08 +0200

    Latest results and tests

--- CUP_holdout.py
+++ CUP_holdout.py
@@ -26,10 +26,11 @@ def CUP_evaluation():
     net = Network("CUP", MEE(), nesterov=True, momentum=0.25)
     net.add(FullyConnectedLayer(10, 23, Tanh(), initialization_func="normalized_xavier"))
     net.add(FullyConnectedLayer(23, 2, initialization_func="normalized_xavier"))
-    history, val_history = net.training_loop(Xtr, Ytr, X_validation=Xvl, Y_validation=Yvl, epochs=4000, learning_rate=0.0025, verbose=True, batch_size=16, early_stopping=50)
+    history, val_history = net.training_loop(Xtr, Ytr, X_validation=Xvl, Y_validation=Yvl, epochs=4000, learning_rate=0.00125, verbose=True, batch_size=16, early_stopping=50)
 
     # Model evaluation
     err = MeanEuclideanError().compute(net, Xts, Yts)
+    plot_and_save(title=suffix, history=history, validation_history=val_history, ylabel="Loss", xlabel="Epochs", savefile=suffix + "_history")
     return err, net, history, val_history
 
 
@@ -42,28 +43,31 @@ err = MeanEuclideanError().compute(newnet, Xts, Yts)
 print("Current best: ", err)
 print("Assessing given network on CUP" )
 vals = []
-hists, val_hists = [], []
-for i in range(5):
-    suffix = "CUP_nesterov/CUP_nesterov.5_1L_23U_eval_" + str(i)
+hists, val_hists, epochs = [], [], []
+for i in range(10):
+    suffix = "CUP_nesterov/CUP_nesterov.25_1L_23U_eval_" + str(i)
     err, net, hist, val_hist = CUP_evaluation()
-    print(err)
+    print("MEE:", err)
     vals.append(err)
     hists.append(hist)
     val_hists.append(val_hist)
     epochs.append(len(hist) - 50)
 
 net.summary()
+#net.savenet("models/CUP_nesterovbest.pkl")
 #plot_and_save(title=suffix, history=history, validation_history=val_history, ylabel="Loss", xlabel="Epochs", savefile=suffix + "_history")
 print("MEE", np.mean(vals), "+-", np.std(vals))
 print("Epochs", np.mean(epochs), "+-", np.std(epochs))
 
 """
 +==== Structure
-|       1. FullyConnectedLayer of 10 -> 23 units with Tanh activation function
-|       2. FullyConnectedLayer of 23 -> 2 units
+|IN     FullyConnectedLayer: 10 units with Tanh activation function
+|HID    FullyConnectedLayer: 23 units with Tanh activation function
+|OUT    FullyConnectedLayer: 2 units
 +==== Loss: Mean Euclidean Error and momentum = 0.25 and Nesterov momentum
 For a total of 301 trainable parameters
-MEE 0.19748257977351702 +- 0.015059235424371582
-Learning rate = 0.0025
+MEE 0.19218554503029459 +- 0.007820414363134251
+Epochs 1290.9 +- 467.80582510268084
+Learning rate = 0.00125
 Batch size = 16
 """
