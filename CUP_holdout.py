962ea0c5593ae7606e6444b8864d0985a81b8325
Federico Matteoni <fedex15@live.it>
2022-05-31 10:16:36 +0200

    Final model? #50

--- CUP_holdout.py
+++ CUP_holdout.py
@@ -22,16 +22,15 @@ def CUP_evaluation():
     Xtr, means, std = continuous_standardizer(Xtr)
 
     # Training
-    Xtr, Xvl, Ytr, Yvl = tr_vl_split(Xtr, Ytr, ratio=0.1)
     net = Network("CUP", MEE(), nesterov=True, momentum=0.25)
     net.add(FullyConnectedLayer(10, 23, Tanh(), initialization_func="normalized_xavier"))
     net.add(FullyConnectedLayer(23, 2, initialization_func="normalized_xavier"))
-    history, val_history = net.training_loop(Xtr, Ytr, X_validation=Xvl, Y_validation=Yvl, epochs=4000, learning_rate=0.00125, verbose=True, batch_size=16, early_stopping=50)
+    history = net.training_loop(Xtr, Ytr, epochs=1549, learning_rate=0.00125, verbose=True, batch_size=16)
 
     # Model evaluation
     err = MeanEuclideanError().compute(net, Xts, Yts)
-    plot_and_save(title=suffix, history=history, validation_history=val_history, ylabel="Loss", xlabel="Epochs", savefile=suffix + "_history")
-    return err, net, history, val_history
+    plot_and_save(title=suffix, history=history, ylabel="Loss", xlabel="Epochs", savefile=suffix + "_history")
+    return err, net, history
 
 
 newnet = Network("Current Best")
@@ -44,17 +43,16 @@ print("Current best: ", err)
 print("Assessing given network on CUP" )
 vals = []
 hists, val_hists, epochs = [], [], []
-for i in range(10):
-    suffix = "CUP_nesterov/CUP_nesterov.25_1L_23U_eval_" + str(i)
-    err, net, hist, val_hist = CUP_evaluation()
+for i in range(25):
+    suffix = "CUP_nesterov/CUP_nesterovbest_eval" + str(i)
+    err, net, hist = CUP_evaluation()
     print("MEE:", err)
     vals.append(err)
     hists.append(hist)
-    val_hists.append(val_hist)
     epochs.append(len(hist) - 50)
 
 net.summary()
-#net.savenet("models/CUP_nesterovbest.pkl")
+net.savenet("models/CUP_nesterovbest.pkl")
 #plot_and_save(title=suffix, history=history, validation_history=val_history, ylabel="Loss", xlabel="Epochs", savefile=suffix + "_history")
 print("MEE", np.mean(vals), "+-", np.std(vals))
 print("Epochs", np.mean(epochs), "+-", np.std(epochs))
@@ -66,8 +64,8 @@ print("Epochs", np.mean(epochs), "+-", np.std(epochs))
 |OUT    FullyConnectedLayer: 2 units
 +==== Loss: Mean Euclidean Error and momentum = 0.25 and Nesterov momentum
 For a total of 301 trainable parameters
-MEE 0.19218554503029459 +- 0.007820414363134251
-Epochs 1290.9 +- 467.80582510268084
+MEE 0.18739041656939193 +- 0.008833923071037302
+Epochs 1549
 Learning rate = 0.00125
 Batch size = 16
 """
