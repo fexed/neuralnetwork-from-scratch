Fixed: loss computation and Binary Cross Entropy

losses.py
@@ -47,12 +47,12 @@ class BinaryCrossentropy(Loss):
 
     def forward(self, labels, outputs):
         outputs_clipped = np.clip(outputs, 1e-15, 1-1e-15)  # avoids div by 0
-        return -np.mean((1 - labels) * np.log(1 - outputs_clipped) + labels * np.log(outputs_clipped))
+        return np.mean(-(1 - labels) * np.log(1 - outputs_clipped) - labels * np.log(outputs_clipped))
 
 
     def derivative(self, labels, outputs):  # TODO check this
         outputs_clipped = np.clip(outputs, 1e-15, 1-1e-15)  # avoids div by 0
-        return np.mean(((1-labels)/(1-outputs_clipped) - labels/outputs_clipped))
+        return np.mean(labels/outputs_clipped - (1-labels)/(1-outputs_clipped))
 
 
 class MulticlassCrossentropy(Loss):