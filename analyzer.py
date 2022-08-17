b2fc4715a669138152df65b8309aa50d8f519ec6
Federico Matteoni <fedex15@live.it>
2022-08-17 18:05:33 +0200

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -11,16 +11,27 @@ with open(filename, 'r') as f:
         except ValueError:
             continue
 
+dir = sys.argv[1][:-11]
+
 print("***BEST RESULTS***")
 print("Best metric")
 bestMetric = sorted(res, key=lambda x: x[1])[:100]
 for r in bestMetric[:10]:
     print(r)
+    with open(dir + str(r[0]) + "_0/logs/MLP.txt", 'r') as f:
+        for line in f:
+            print(line, end="")
+    print("\n")
+
 print("\n")
 print("Lowest std dev")
 lowestStdDev = sorted(res, key=lambda x: x[2])[:100]
 for r in lowestStdDev[:10]:
     print(r)
+    with open(dir + str(r[0]) + "_0/logs/MLP.txt", 'r') as f:
+        for line in f:
+            print(line, end="")
+    print("\n")
 
 bestModels = []
 for elem in bestMetric:
@@ -28,10 +39,15 @@ for elem in bestMetric:
         if elem[0] == tgt[0]:
             bestModels.append(elem)
             break
-
 print("\n")
 print("Best models")  # common between the bestMetric and lowestStdDev
 if len(bestModels) == 0: print("No best models")
 else:
     for r in bestModels[:5]:
         print(r)
+        with open(dir + str(r[0]) + "_0/logs/MLP.txt", 'r') as f:
+            for line in f:
+                print(line, end="")
+        print("\n")
+
+
